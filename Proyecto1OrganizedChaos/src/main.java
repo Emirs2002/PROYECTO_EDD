f43059d1f33a06bee062e656b861d59242a6bea3
Emily <em.rodriguez@correo.unimet.edu.ve>
2022-05-23 02:09:20 -0400

    Inicio

---
+++ Proyecto1OrganizedChaos/src/main.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author ERodríguez
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
